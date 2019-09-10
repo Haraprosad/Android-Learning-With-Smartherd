0d182ee293882ec0e3aba814843a6aabd84560d3
Haraprosad Biswas <haraprosadbiswas123@gmail.com>
2019-09-10 12:32:25 +0600

    2.Explicit Intent

--- app/src/main/java/org/LingJ/msgshareapp/MainActivity.kt
+++ app/src/main/java/org/LingJ/msgshareapp/MainActivity.kt
@@ -25,18 +25,44 @@ class MainActivity:AppCompatActivity() {
             /*To get text from edit text -->input and convert edit text to String*/
 
             val message:String = etWriteMessage.text.toString()
+            /*Intent has 2 types. i)Explicit : share information to known activiry
+           * ii)Implicit: share information to unknown activity*/
+
+            /*Use of Explicit Intent */
 
             /*Transition from one activity to another activity.*/
             val i = Intent(this,SecondActivity::class.java)
-
-            /*Intent has 2 types. i)Explicit : share information to known activiry
-            * ii)Implicit: share information to unknown activity*/
 
             /*Use of Explicit Intent:
             * use putExtra(key_name,value) method to send information to
             * intent object*/
             i.putExtra("user_msg",message)
             startActivity(i)
+        }
+        btnShareMsgToAnotherApp.setOnClickListener {
+            val message:String = etWriteMessage.text.toString()
+
+            /****************************************************/
+
+            /*Use of Explicit Intent:*/
+            /*Step-1:Intent type object creation
+            * Step-2: activities specification with object-
+            *         (i)send action
+            *          (ii)value assign
+            *           (iii)Value's type
+            * Step-3:Start activity */
+            val i = Intent()   //as we don't know the exact send destination
+            i.action = Intent.ACTION_SEND //To start send action
+            i.putExtra(Intent.EXTRA_TEXT,message) // put msg in EXTRA_TEXT key
+
+            /*EXTRA_TEXT key is the specific key name which will get called
+            * by sharing app (viz:fb,whatsapp,gmail etc)*/
+
+            i.type ="text/plain" //value's attribute
+            /*To open sharing app list and show the title: viz: Send To*/
+            startActivity(Intent.createChooser(i,"Send To:"))
+
+            /****************************************************/
         }
     }
 
